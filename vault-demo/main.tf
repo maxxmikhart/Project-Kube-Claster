6eba1a5e5730cc3d8e7bc2b465f68e5cfc1ae1cc
farrukh90 <farrukhsadykov@gmail.com>
2023-04-10 17:36:10 -0500

    updated main

--- vault-demo/main.tf
+++ vault-demo/main.tf
@@ -4,52 +4,55 @@ resource "random_password" "password" {
   numeric = true
 }
 
-
-
-resource "vault_mount" "kvv2" {
-  path        = "us/development"
-  type        = "kv"
-  options     = { version = "2" }
-  description = "KV Version 2 secret engine mount"
+resource "vault_mount" "db-team" {
+  path        = "db"
+  type        = "kv-v2"
+  description = "This is an example KV Version 2 secret engine mount"
 }
 
 
-resource "vault_mount" "kvv2us" {
-  path        = "eu/development/"
-  type        = "kv"
-  options     = { version = "2" }
-  description = "KV Version 2 secret engine mount"
-}
 
-resource "vault_kv_secret_v2" "secret" {
-  mount = "eu/development/"
-  name  = "wordpress"
+
+resource "vault_generic_secret" "secret" {
+  path = "${vault_mount.db-team.path}/db-usernames"
   data_json = jsonencode(
     {
-      password = random_password.password.result
+      username = "admin",
+      password = "${random_password.password.result}"
     }
   )
 }
 
 
 
-# resource "google_sql_database_instance" "wordpress" {
-#   name                = "wordpress"
-#   database_version    = "MYSQL_5_7"
-#   region              = "us-central1"
-#   deletion_protection = false
-
-#   settings {
-#     # Second-generation instance tiers are based on the machine
-#     # type. See argument reference below.
-#     tier = "db-f1-micro"
-#   }
-# }
 
 
-# resource "google_sql_user" "users" {
-#   name     = "wordpress"
-#   instance = google_sql_database_instance.wordpress.name
-#   host     = "wordpress.${var.google_domain_name}"
-#   password = random_password.password.result
-# }
+
+
+
+
+
+
+
+
+
+resource "google_sql_database_instance" "wordpress" {
+  name                = "wordpress"
+  database_version    = "MYSQL_5_7"
+  region              = "us-central1"
+  deletion_protection = false
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+}
+
+
+resource "google_sql_user" "users" {
+  name     = "wordpress"
+  instance = google_sql_database_instance.wordpress.name
+  host     = "wordpress.${var.google_domain_name}"
+  password = random_password.password.result
+}
